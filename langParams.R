13db7899faf60a9d4e73258fa53b3427fc9f4a76
Jean-Mathieu Potvin <jeanmathieupotvin@ununoctium.dev>
2025-01-22 15:15:25 -0500

    Deactivation of translation mechanisms: gett(), and gettt().
    They were deactivated for multiple reasons.
    
    1. Sys.setlocale() systematically throws an error on Ubuntu 24.04.
       OS refuses to change the locale of 'LC_MESSAGES'.
    2. It is easier to match what we see (live in the app) with what is
       in the source code by deactivating translation of msgids. gett()
       and gettt() now simply returns their argument as is, which is the
       value of msgid (they are identity functions).
    
    They will be removed in the future and replaced by transltr.

--- langParams.R
+++ langParams.R
@@ -1,16 +1,27 @@
 transDomain <- "default"
 transDir <- "./Rtrans"
-loc <- "en_CA"
-Sys.setlocale("LC_MESSAGES", loc)
 
-gett <- function(msgid)
-{
-  bindtextdomain(transDomain, transDir)
-  return(gettext(msgid, domain=transDomain))
-  #return(loc)
-}
+# NOTE: (JMP) Sys.setlocale() throws an error on Ubuntu 24.04.
+# Since NLS is deactivated below, I also deactivated these calls until
+# further notice.
+#loc <- "en_CA"
+#Sys.setlocale("LC_MESSAGES", loc)
 
-gettt <- function(msgid, ...)
-{
-  return(sprintf(gett(msgid), ...))
-}
+
+# NOTE: (JMP) gett() and gettt() will soon be deprecated and are unstable
+# functions that yield errors on Ubuntu 24.04. I replaced them by identity
+# functions that simply returns the source text's IDs until further notice.
+gett  <- \(msgid, ...) return(msgid)
+gettt <- gett
+
+# gett <- function(msgid)
+# {
+#   bindtextdomain(transDomain, transDir)
+#   return(gettext(msgid, domain=transDomain))
+#   #return(loc)
+# }
+
+# gettt <- function(msgid, ...)
+# {
+#   return(sprintf(gett(msgid), ...))
+# }
