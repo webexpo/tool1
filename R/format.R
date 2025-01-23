d1a293be71667fdb67e4a1334481b01ef30f5520
Jean-Mathieu Potvin <jeanmathieupotvin@ununoctium.dev>
2025-01-23 16:36:42 -0500

    Better error handling for as_percentage().

--- R/format.R
+++ R/format.R
@@ -13,10 +13,21 @@
 #' as_percentage(c(a = 1.34452, b = 56.31))
 #'
 #' @author Jean-Mathieu Potvin (<jeanmathieupotvin@@ununoctium.dev>)
-as_percentage <- function(values = numeric()) {
-    stopifnot(
-        is.numeric(values),
-        !anyNA(values))
+as_percentage <- function(values = numeric(), digits = 3L) {
+    if (!is.numeric(values) || anyNA(values)) {
+        stop(
+            "'values' must be a numeric vector of non-NA values.",
+            call. = FALSE)
+    }
+    if (!is.integer(digits) ||
+        length(digits) != 1L ||
+        is.na(digits) ||
+        !is.finite(digits) ||
+        digits < 0L) {
+        stop(
+            "'digits' must be a finite and non-NA integer value greater than 0.",
+            call. = FALSE)
+    }
 
-    return(structure(sprintf("%.3f%%", values), names = names(values)))
+    return(structure(sprintf("%.*f%%", digits, values), names = names(values)))
 }
