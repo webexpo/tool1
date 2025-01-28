3cdea6e88a9318897f39fbcb2cbc31913bd79d41
Jean-Mathieu Potvin <jeanmathieupotvin@ununoctium.dev>
2025-01-28 13:28:49 -0500

    Changes to as_percentage(). Still experimental.
    Other existing formatting functions were moved in this script (temporary).

--- R/format.R
+++ R/format.R
@@ -6,6 +6,12 @@
 #'
 #' @param values A [numeric] vector, possibly empty. `NA` values are disallowed.
 #'
+#' @param digits A non-`NA` [integer] value greater than 0. Values are rounded
+#'   up to a precision of `digits`.
+#'
+#' @param verbose A non-`NA` [logical] value. Should `values` and formatted
+#'   values be reported to the console?
+#'
 #' @returns
 #' A character vector having the same length as `values`. Names are preserved.
 #'
@@ -13,7 +19,11 @@
 #' as_percentage(c(a = 1.34452, b = 56.31))
 #'
 #' @author Jean-Mathieu Potvin (<jeanmathieupotvin@@ununoctium.dev>)
-as_percentage <- function(values = numeric(), digits = 3L) {
+as_percentage <- function(
+    values  = numeric(),
+    digits  = 3L,
+    verbose = getOption("tool1.verbose.percentage", FALSE))
+{
     if (!is.numeric(values) || anyNA(values)) {
         stop(
             "'values' must be a numeric vector of non-NA values.",
@@ -28,6 +38,36 @@ as_percentage <- function(values = numeric(), digits = 3L) {
             "'digits' must be a finite and non-NA integer value greater than 0.",
             call. = FALSE)
     }
+    if (!is.logical(verbose) || length(verbose) != 1L || is.na(verbose)) {
+        stop(
+            "'verbose' must be a non-NA logical of length 1 ('TRUE' or 'FALSE').",
+            call. = FALSE)
+    }
 
-    return(structure(sprintf("%.*f%%", digits, values), names = names(values)))
+    out <- structure(sprintf("%.*f%%", digits, values), names = names(values))
+
+    if (verbose) {
+        mapply(input = values, output = out, \(input, output) {
+            cat("[NOTE]", "[as_percentage()]", input, "->", output, "\n",
+                sep = " ")
+        })
+    }
+
+    return(out)
+}
+
+percText <- function(perc) {
+    rem <- perc %% 10
+    if ( rem == 1) {
+        suff <- gett("st")
+    } else
+        if ( rem == 2 ) {
+        suff <- gett("nd")
+    } else
+    if ( rem == 3 ) {
+        suff <- gett("rd")
+    } else {
+        suff <- gett("th")
+    }
+    return (paste0(perc, suff, " ", gett("percentile") ) )
 }
