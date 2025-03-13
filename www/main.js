0445e5619a5ef2848a2bc42db7cf9cf723d177a9
Jean-Mathieu Potvin <jeanmathieupotvin@ununoctium.dev>
2025-03-13 14:15:14 -0400

    Add log information to js handlers for debugging purposes.

--- www/main.js
+++ www/main.js
@@ -4,18 +4,19 @@
 // are considered valid (and checked by the server before).
 
 // Update the window's title (what is displayed in a browser's tab).
-Shiny.addCustomMessageHandler(
-  "update_window_title",
-  (x) => (document.title = x)
-);
+Shiny.addCustomMessageHandler("update_window_title", (title) => {
+  document.title = title;
+  console.log("[INFO] Updating window's title.");
+});
 
 // Update the value of attribute "lang" of <html> element (the web page).
-Shiny.addCustomMessageHandler(
-  "update_page_lang",
-  (x) => (document.documentElement.lang = x)
-);
+Shiny.addCustomMessageHandler("update_page_lang", (lang) => {
+  document.documentElement.lang = lang;
+  console.log(`[INFO] Updating attribute lang of root element to ${lang}.`);
+});
 
 // Update the value of attribute named "attr" of element identified by "id".
 Shiny.addCustomMessageHandler("update_attribute", ({ id, attr, value }) => {
   document.getElementById(id).setAttribute(attr, value);
+  console.log(`[INFO] Updating attribute ${attr} of #${id} to ${value}.`);
 });
