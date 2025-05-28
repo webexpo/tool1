804df7fe869620905c2998cdf179a275fce8336b
Jean-Mathieu Potvin <jeanmathieupotvin@ununoctium.dev>
2025-05-28 15:11:37 -0400

    Bugfix: find-text.R script was not dropping older translations.
    The logic of this script had to be rewritten. The previous
    implementation was not dropping older (unused) pairs of source
    text and translation. Instead, translation files were accumulating
    them. This was problematic because whenever the source text had to
    be modified, olver versions would be kept. Translation files became
    bloated and messy.
    
    The new version creates a fresh Translator object from the source
    code and then import back translations that are still relevent.
    Those that are not are discarded when translator_write() is called
    (files are overwritten).

--- .scripts/find-text.R
+++ .scripts/find-text.R
@@ -18,21 +18,40 @@
 #'
 #' @author Jean-Mathieu Potvin (<jeanmathieupotvin@@ununoctium.dev>)
 
-# Use the existing Translator object, or create one if it does not exist.
-tr <- if (utils::file_test("-f", getOption("transltr.path"))) {
-    transltr::translator_read()
-} else {
-    transltr::translator(id = "expostats:tool1")
-}
+# Create a new (empty) Translator object.
+tr <- transltr::translator(id = "expostats:tool1")
 
 # Register languages that must be supported.
+# Each named element is interpreted as <code> = <displayed name>.
 tr$set_native_languages(
     en = "English",
     fr = "Français"
 )
 
-# Extract source text to translate from source scripts.
+# Extract source text to translate from source scripts
+# and update the Translator object (by reference).
 transltr::find_source(tr = tr, interface = quote(translate))
+
+# The following steps are done in a local environment to avoid
+# polluting the current environment with temporary variables.
+local({
+    # Get the directory holding translations.
+    dir <- dirname(getOption("transltr.path"))
+
+    # Extract non-source language codes that may
+    # have an existing translation file already.
+    langs <- names(tr$native_languages)
+    langs <- langs[langs != transltr::language_source_get()]
+
+    # Only keep files that exists.
+    files <- file.path(dir, sprintf("%s.txt", langs))
+    files <- files[utils::file_test("-f", files)]
+
+    # Read existing translations and import them
+    # back into the Translator object created above.
+    # The latter is updated by reference.
+    lapply(files, transltr::translations_read, tr = tr)
+})
 
 # Export source text and translations.
 # This populates the contents of intl/.
