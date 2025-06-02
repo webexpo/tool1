40cd5d0a878a3ad47ae4fb6ab38782c0080ff73c
Jean-Mathieu Potvin <jeanmathieupotvin@ununoctium.dev>
2025-06-02 11:06:05 -0400

    Update release date of version 5.1.0.

--- R/global.R
+++ R/global.R
@@ -44,7 +44,7 @@ shiny::shinyOptions(
 # Constants --------------------------------------------------------------------
 
 # Default version/release to display in footers.
-default_version <- c(number = "5.1.0", release_date = "2025-05-29")
+default_version <- c(number = "5.1.0", release_date = "2025-06-02")
 
 # Default language.
 default_lang <- transltr::language_source_get()
